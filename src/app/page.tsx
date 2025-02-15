4202792de7da73b695769ce151dd1915c33c62e7
Akhil Waghmare <akhilwaghmare@gmail.com>
2025-02-14 17:16:02 -0800

    Starter code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
 import { ArrowUp } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
@@ -14,44 +13,15 @@ export default function Home() {
     "Teach me to negotiate",
     "Design a database schema",
   ];
-  const [sampleInputIndex, setSampleInputIndex] = useState(0);
-
-  useEffect(() => {
-    if (userInput.length > 0) return;
-
-    const interval = setInterval(() => {
-      setSampleInputIndex((prev) => (prev + 1) % sampleInputs.length);
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, [userInput]);
 
   return (
     <div className="flex flex-col justify-center items-center gap-12 min-h-screen max-w-[80%] mx-auto p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="font-medium text-3xl">What can I help with?</h1>
       <div className="relative bg-muted rounded-xl w-full p-3">
-        <div
-          className="[&[data-feedback=true]]:opacity-0"
-          data-feedback={userInput ? "true" : "false"}
-        >
-          <AnimatePresence mode="popLayout" initial={false}>
-            <motion.span
-              aria-hidden
-              className="absolute left-3 top-3 w-full text-muted-foreground"
-              key={sampleInputIndex}
-              initial={{ y: 20, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: -20, opacity: 0 }}
-              transition={{ type: "spring", bounce: 0 }}
-            >
-              {sampleInputs[sampleInputIndex]}
-            </motion.span>
-          </AnimatePresence>
-        </div>
         <form>
           <textarea
             rows={3}
-            className="w-full resize-none outline-none [&::placeholder]:opacity-0 bg-transparent"
+            className="w-full resize-none outline-none bg-transparent"
             autoFocus
             placeholder="Enter query"
             onChange={(e) => setUserInput(e.target.value)}
